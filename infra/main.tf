e52e377c17f39a9be5490a270b52ee6b2f189d03
Lucas Silva <lucasisrael85@gmail.com>
2025-09-19 11:29:33 -0300

    correcao terraform route table

--- infra/main.tf
+++ infra/main.tf
@@ -6,28 +6,37 @@ resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
   tags = {
     Name = "main_vpc"
-    }
+  }
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main-igw"
+  }
 }
 
 resource "aws_subnet" "main" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.1.0/24"
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
   tags = {
     Name = "main_subnet"
-    }
+  }
 }
 
 resource "aws_route_table" "main" {
   vpc_id = aws_vpc.main.id
-  
+
   route {
-    cidr_block = "0.0.0.0./0"
-    gateway_id = aws_internet_gateway.main.id 
-    }
-    tags = {
-      Name = "main_route_table"
-    }
+    # Note que há um ponto a mais no seu CIDR, o correto é 0.0.0.0/0
+    cidr_block = "0.0.0.0/0" 
+    gateway_id = aws_internet_gateway.main.id # Agora esta referência funciona!
+  }
+  tags = {
+    Name = "main_route_table"
+  }
 }
 
 resource "aws_route_table_association" "a" {
